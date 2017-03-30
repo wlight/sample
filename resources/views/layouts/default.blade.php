204a55c08510d37d7ffe33bb581f04b7774e6711
wlight <wormside@gmail.com>
2017-03-30 13:59:52 +0000

    Finish layout and routes

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -3,8 +3,13 @@
   <head>
     <meta charset="utf-8">
     <title>@yield('title', 'Sample App') - Laravel第一个程序</title>
+    <link rel="stylesheet" href="/css/app.css">
   </head>
   <body>
-    @yield('content')
+    @include('layouts._header')
+    <div class="container">
+      @yield('content')
+      @include('layouts._footer')
+    </div>
   </body>
 </html>
