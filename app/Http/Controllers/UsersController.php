9aae0888f704ecbfc8f12b980a5f97b47a4727ff
wlight <wormside@gmail.com>
2017-03-31 16:09:10 +0000

    Sign-up Form

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -7,11 +7,18 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
+use App\Models\User;
+
 class UsersController extends Controller
 {
     //
     public function create ()
     {
         return view('users.create');
+    }
+    public function show ($id)
+    {
+        $user = User::findOrFail($id);
+        return view('users.show', compact('user'));
     }
 }
--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -2,5 +2,36 @@
 @section('title', '注册')
 
 @section('content')
-<h1>注册</h1>
+<div class="col-md-offset-2 col-md-8">
+  <div class="panel panel-default">
+    <div class="panel-heading">
+      <h5>注册</h5>
+    </div>
+    <div class="panel-body">
+      <form class="" action="{{ route('users.store') }}" method="post">
+        <div class="form-group">
+          <label for="name">名称：</label>
+          <input type="text" name="name" value="{{ old('name') }}" class="form-control">
+        </div>
+
+        <div class="form-group">
+          <label for="email">邮箱：</label>
+          <input type="text" name="email" value="{{ old('email') }}" class="form-control">
+        </div>
+
+        <div class="form-group">
+          <label for="password">密码：</label>
+          <input type="password" name="password" value="{{ old('password') }}" class="form-control">
+        </div>
+
+        <div class="form-group">
+          <label for="password_confirmation">确认密码：</label>
+          <input type="password" name="password_confirmation" value="{{ old('password_confirmation') }}" class="form-control">
+        </div>
+
+        <button type="submit" class="btn btn-primary">注册</button>
+      </form>
+    </div>
+  </div>
+</div>
 @stop
