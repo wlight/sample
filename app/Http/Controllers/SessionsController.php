adfca597d764b91f080a21bf4d50f214ba1f173b
wlight <wormside@gmail.com>
2017-04-03 15:02:14 +0000

    Finsh logout first step

---
+++ app/Http/Controllers/SessionsController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class SessionsController extends Controller
+{
+    public function create ()
+    {
+        return view('sessions.create');
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'email' => 'required|email|max:255',
+            'password' => 'required'
+        ]);
+        return;
+    }
+}
